f3126d578814f741944f5dacbc7e4de08c7d7f87
LoArt & Dev <35228419+SimonLopezJara@users.noreply.github.com>
2023-03-07 12:16:07 -0500

    0.1.3 (Google Analytics)

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,8 @@ module.exports = {
     siteUrl: process.env.WEB_URL,
   },
   plugins: [
+    `gatsby-plugin-mdx`,
+    `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -34,6 +36,40 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     'gatsby-plugin-postcss',
-    'gatsby-plugin-tailwindcss'
+    'gatsby-plugin-tailwindcss',
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-3SK66T1MHQ", // Google Analytics / GA
+          //"AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          //"DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/dev/**", "/admin/**"],
+          // Defaults to https://www.googletagmanager.com
+          //origin: "YOUR_SELF_HOSTED_ORIGIN",
+          // Delays processing pageview events on route update (in milliseconds)
+          delayOnRouteUpdate: 0,
+        },
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `LoArt & Dev`,
+        short_name: `LoArtDev`,
+        start_url: `/`,
+        background_color: `#0d0d0d`,
+        theme_color: `#dbdbdb`,
+        display: `standalone`,
+      },
+    },
   ],
 };
