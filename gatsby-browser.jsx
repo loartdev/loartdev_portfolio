afef765c685f019ac141278e9ff6e7ec45b70367
LoArt & Dev <35228419+SimonLopezJara@users.noreply.github.com>
2023-03-07 00:25:57 -0500

    0.1.2 (vercel analytics)

--- gatsby-browser.jsx
+++ gatsby-browser.jsx
@@ -1,3 +1,5 @@
 import './src/styles/global.css' // App styles
-
+// import { inject } from '@vercel/analytics'; // vercel analytics
 const React = require("react")
+
+// inject();
--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Nav from "./nav";
 import Footer from '../components/footer';
+import { Analytics } from '@vercel/analytics/react';
+
 
 function Layout({ children }) {
   return (
     <>
       <Nav />
       {children}
+      <Analytics />
       <Footer />
     </>
 
